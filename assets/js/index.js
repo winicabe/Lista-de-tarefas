0a57b39f0adf5b003d13e8a1476534d78fc9a2eb
winicabe <winicius._cavalcante@hotmail.com>
2022-01-10 20:55:37 -0300

    adicionando CSS

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,13 +11,14 @@ const Task = {
     addTask(task_name){
         this.tasks.push({name:task_name});
         App.reload()
+
     },
     deleteTask(index){
         this.tasks.splice(index, 1);
         App.reload()
     },
     getTaskName(){
-        task_name = document.getElementById("taskname").value
+        task_name = document.getElementById("task-name").value
         return task_name
     }
 }
@@ -25,21 +26,25 @@ const DOM = {
     container : document.querySelector("#task-list"),
     addTask(task_name, index) {
         const list_box = document.createElement("div")
-        list_box.dataset.index = index;
+        //list_box.dataset.index = index;
         list_box.innerHTML = this.buildHTML(task_name.name, index);
         this.container.appendChild(list_box)
+        this.clearInput()
     },
     buildHTML(task_name, index){
         html = `
         <h2 class="title-task">${task_name}</h2>
-        <button onclick="Task.deleteTask(${index})" class="delete">Delete</button>`;
+        <img onclick="Task.deleteTask(${index})" class="delete" src="https://img.icons8.com/material-outlined/48/000000/trash--v1.png"/>`;
         return html;
     },
     inputUser(){
-        return user_input = document.getElementById("taskname").value;
+        return user_input = document.getElementById("task-name").value;
     },
     clearHTML(){
         this.container.innerHTML = ""
+    },
+    clearInput(){
+        user_input = document.getElementById("task-name").value = "";
     }
 }
 const App = {
@@ -56,25 +61,3 @@ const App = {
     }
 }
 App.run()
-// function addTask(user_input) {
-//     //nome da tarefa.
-//     const layout = document.createElement("div");
-//     const task = document.createElement("h1");
-//     const bt_delete = document.createElement("button");
-//     const task_name = document.createTextNode(user_input.value);
-//     //const line = document.createElement("hr");
-//     bt_delete.innerHTML = "delete"
-//     //adicionando componentes
-//     layout.appendChild(task);
-//     layout.appendChild(bt_delete);
-//     task.appendChild(task_name);
-//     document.body.appendChild(layout);
-//     //layout.appendChild(line)
-//     //removendo tarefas
-//     bt_delete.onclick = function() {
-//         layout.remove();
-//     }
-    
-// };
-// adicionar = document.getElementById("addbutton")
-// adicionar.onclick = function() {addTask(user_input);};
